b2cb715efbc3b91ed5ffc2888e5ccd98e287c81c
wsyski <wsyski@gmail.com>
2019-09-08 19:12:46 +0200

    Initial version

---
+++ bin/generate-bibframe.sh
@@ -0,0 +1,2 @@
+/rdf4j-schema-generator /home/wos/playground/java/rdf/rdf/src/main/resources/bibframe.rdf -f 'application/rdf+xml' -u 'http://id.loc.gov/ontologies/bibframe/' BIBFRAME.java
+
---
+++ bin/generate-schema.sh
@@ -0,0 +1,2 @@
+./rdf4j-schema-generator /home/wos/playground/java/rdf/rdf/src/main/resources/schema.rdf -f 'application/rdf+xml' -u 'http://schema.org/' SCHEMA.java
+
